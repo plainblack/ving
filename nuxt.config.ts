775ca0626e8375842d237ddf0f380cfba99e9a15
JT Smith <jt@plainblack.com>
2023-02-20 19:42:26 -0600

    Removing web tokens and nuxt auth

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    runtimeConfig: {
-        accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
-        refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET
-
-    }
 
 })
