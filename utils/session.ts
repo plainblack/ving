d01559f5bff15ebcb856cfa06ddad7e3e6892452
JT Smith <jt@plainblack.com>
2023-02-20 21:16:37 -0600

    built session

--- utils/session.ts
+++ utils/session.ts
@@ -1,9 +1,11 @@
 import { Users, UserRecord } from './db';
+import { Ouch } from './utils';
+import { cache } from './cache';
 import crypto from 'crypto';
 
 export class Session {
 
-    constructor(public userId: string, private passwordHash: string, public expires = new Date(), public id = crypto.randomUUID()) { }
+    constructor(public userId: string, private passwordHash?: string, public id = crypto.randomUUID()) { }
 
     private userObj: UserRecord | undefined;
 
@@ -17,9 +19,34 @@ export class Session {
         return this.userObj = await Users.findUnique({ where: { id: this.userId } });
     }
 
-    private get key() {
-        return 'session-' + this.id;
+    public async end() {
+        await cache.delete('session-' + this.id);
     }
 
+    public async extend() {
+        const user = await this.user();
+        if (this.passwordHash != user.props.password) { // password changed since session created
+            this.end();
+            return;
+        }
+        await cache.set('session-' + this.id, {
+            userId: this.userId,
+            passwordHash: this.passwordHash,
+        }, 1000 * 60 * 60 * 24 * 7);
+    }
+
+    static start(user: UserRecord) {
+        const session = new Session(user.props.id as string, user.props.password || '');
+        session.extend();
+        return session;
+    }
+
+    static async fetch(id: string) {
+        const data: { userId: string, passwordHash: string } | undefined = await cache.get('session-' + id);
+        if (data !== undefined) {
+            return new Session(data.userId, data.passwordHash, id);
+        }
+        throw new Ouch(451, 'Session expired.');
+    }
 
 }
