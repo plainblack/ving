025b8ca2b62c2a93b94499e864d21981222f23d0
JT Smith <jt@plainblack.com>
2023-02-23 23:43:32 -0600

    can edit check

--- server/api/user/[id].put.ts
+++ server/api/user/[id].put.ts
@@ -3,6 +3,7 @@ import { vingDescribe, vingSession, vingBody } from '~~/utils/helpers';
 export default defineEventHandler(async (event) => {
     const { id } = getRouterParams(event);
     const user = await Users.find(id);
+    user.canEdit(vingSession(event));
     await user.updateAndVerify(await vingBody(event), vingSession(event));
     return user.describe(vingDescribe(event));
 });
