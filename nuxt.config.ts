781caadcb4d1032a7b7dd95de4e9e9ad9f663962
JT Smith <jt@plainblack.com>
2023-02-24 22:54:57 -0600

    added font awesome

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+            link: [
+                { // pro subscription to font-awesome, will only work on my domains and localhost
+                    rel: 'stylesheet',
+                    href: 'https://pro.fontawesome.com/releases/v5.15.4/css/all.css',
+                    integrity: 'sha384-rqn26AG5Pj86AF4SO72RK5fyefcQ/x32DNQfChxWvbXIyXFePlEktwD18fEz+kQU',
+                    crossorigin: 'anonymous'
+                }
+            ]
+        }
+    },
     runtimeConfig: {
     }
 })
