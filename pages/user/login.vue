cabf1d016b0dcd65325cb0ae2b9e9775abc63064
JT Smith <jt@plainblack.com>
2023-02-24 20:25:01 -0600

    got types working in vue

--- pages/user/login.vue
+++ pages/user/login.vue
@@ -3,11 +3,15 @@
     <input type="text" v-model="password">
     {{ login }} {{ password }}
     <button @click="tryLogin">log in</button>
+
+    <p> {{ user.props && user.props.displayName }}</p>
 </template>
 
 <script setup lang="ts">
+import { Describe } from '~/utils/db';
 let login = ref('');
 let password = ref('');
+let user = ref<Describe<'User'>>({ props: {} });
 
 async function tryLogin() {
     const session = await useFetch('/api/session?includeRelated=user', {
@@ -17,9 +21,8 @@ async function tryLogin() {
             password
         }
     });
-    if (session.data.value) {
-        const user = session.data.value.related?.user;
-        console.log(user);
+    if (session.data.value && session.data.value.related && session.data.value.related.user) {
+        user.value = session.data.value.related?.user;
     }
 }
 </script>
