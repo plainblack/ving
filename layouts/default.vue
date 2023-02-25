576f817622514246e5a6f761f88c59480d351cea
JT Smith <jt@plainblack.com>
2023-02-24 23:22:09 -0600

    set up a layout

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>
+            <NuxtLink to="/">
+                <h1>Ving</h1>
+            </NuxtLink>
+            <NuxtLink to="/user/login">
+                <i class="fa-regular fa-circle-user"></i>
+            </NuxtLink>
+        </header>
+        <div>
+            <slot />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
