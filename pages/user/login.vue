6677f04cb74c6e55942ff76c2e3b85fd17f41ec0
JT Smith <jt@plainblack.com>
2023-02-24 18:01:08 -0600

    started building a login form

---
+++ pages/user/login.vue
@@ -0,0 +1,24 @@
+<template>
+    <input type="text" v-model="login">
+    <input type="text" v-model="password">
+    {{ login }} {{ password }}
+    <button @click="tryLogin">log in</button>
+</template>
+
+<script setup lang="ts">
+let login = ref('');
+let password = ref('');
+
+async function tryLogin() {
+    const result = await useFetch('/api/session', {
+        method: 'POST',
+        body: {
+            login,
+            password
+        }
+    })
+    console.log(result)
+}
+</script>
+
+
