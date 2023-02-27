9e148aa8e89d42bbee52a2135a6bf0ad50d7be5a
JT Smith <jt@plainblack.com>
2023-02-27 14:56:26 -0600

    restricted font awesome to only the icons we're using

--- layouts/default.vue
+++ layouts/default.vue
@@ -29,11 +29,7 @@
                     </div>
                 </div>
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                    <button type="button"
-                        class="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span class="sr-only">View notifications</span>
-                        <font-awesome-icon icon="fa-solid fa-bell" class="h-6 w-6" aria-hidden="true" />
-                    </button>
+
 
                     <!-- Profile dropdown -->
                     <Menu as="div" class="relative ml-3">
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -7,8 +7,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 //import { far } from '@fortawesome/free-regular-svg-icons'
 //import { fal } from '@fortawesome/free-light-svg-icons'
 //import { fab } from '@fortawesome/free-brands-svg-icons'
-import { fas } from '@fortawesome/free-solid-svg-icons'
-import { fas as fasPro } from '@fortawesome/pro-solid-svg-icons'
+//import { fas } from '@fortawesome/free-solid-svg-icons'
+//import { fas as fasPro } from '@fortawesome/pro-solid-svg-icons'
 //import { far  } from '@fortawesome/pro-regular-svg-icons'
 //import { fal  } from '@fortawesome/pro-light-svg-icons'
 //import { fat  } from '@fortawesome/pro-thin-svg-icons'
@@ -16,13 +16,16 @@ import { fas as fasPro } from '@fortawesome/pro-solid-svg-icons'
 //import { fass  } from '@fortawesome/sharp-solid-svg-icons'
 //import { fasr  } from '@fortawesome/sharp-regular-svg-icons'
 
+import { faBars, faXmark } from '@fortawesome/free-solid-svg-icons'
+
+
 // This is important, we are going to let Nuxt worry about the CSS
 config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(fas)
-library.add(fasPro)
+//library.add(far)
+library.add(faBars, faXmark)
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
