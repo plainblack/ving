320db5ce5675fbdf24e5297a12b58cf003b3cf2c
JT Smith <jt@plainblack.com>
2023-02-27 23:51:41 -0600

    added change password

--- pages/user/create.vue
+++ pages/user/create.vue
@@ -46,7 +46,9 @@
                                 class="rounded-md bg-red-50 p-4">
                                 <div class="flex">
                                     <div class="flex-shrink-0">
-                                        <XCircleIcon class="h-5 w-5 text-red-400" aria-hidden="true" />
+                                        <font-awesome-icon icon="fa-solid fa-circle-x" class="h-5 w-5 text-red-400"
+                                            aria-hidden="true" />
+
                                     </div>
                                     <div class="ml-3">
                                         <h3 class="text-sm font-medium text-red-800">Your account has not met these
--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -1,6 +1,6 @@
 // For Nuxt 3
-import { library, config } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library, config } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 // if you uncomment any of the below, make sure you add them to the transpile section in nuxt.config.ts
 
@@ -16,16 +16,17 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 //import { fass  } from '@fortawesome/sharp-solid-svg-icons'
 //import { fasr  } from '@fortawesome/sharp-regular-svg-icons'
 
-import { faBars, faXmark, faKey, faCircleUser, faSliders } from '@fortawesome/free-solid-svg-icons'
+import { faBars, faXmark, faKey, faCircleUser, faSliders } from '@fortawesome/free-solid-svg-icons';
+import { faCircleX } from '@fortawesome/pro-solid-svg-icons';
 
 
 // This is important, we are going to let Nuxt worry about the CSS
-config.autoAddCss = false
+config.autoAddCss = false;
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
 //library.add(far)
-library.add(faBars, faXmark, faKey, faCircleUser, faSliders)
+library.add(faBars, faXmark, faKey, faCircleUser, faSliders, faCircleX);
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.component('font-awesome-icon', FontAwesomeIcon, {})
