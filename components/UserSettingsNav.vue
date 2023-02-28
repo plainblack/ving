46291138e2c249c9693f3d899fcfe429d32a1c03
JT Smith <jt@plainblack.com>
2023-02-27 22:44:53 -0600

    removed garbage

--- components/UserSettingsNav.vue
+++ components/UserSettingsNav.vue
@@ -13,7 +13,6 @@
 </template>
 
 <script setup lang="ts">
-//import { CreditCardIcon, KeyIcon, SquaresPlusIcon, UserCircleIcon, UserGroupIcon } from '@heroicons/vue/24/outline'
 const route = useRoute();
 
 const navigation = [
