37b0acf8b79a9981d7cb2815e188dd307af75e81
JT Smith <jt@plainblack.com>
2023-03-02 23:44:04 -0600

    added user profile

---
+++ pages/user/profile/[id].vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="surface-ground px-4 md:px-6 lg:px-8">
+        <div class="p-fluid flex flex-column lg:flex-row">
+            <div class="surface-card p-5 shadow-2 border-round flex-auto">
+                <div class="text-900 font-semibold text-lg mt-3">User profile for {{ user.props?.displayName }}</div>
+                Created on {{ dt.formatDate(user.props?.createdAt) }}
+                <div class="flex gap-5 flex-column-reverse md:flex-row">
+                    <div class="flex-auto p-fluid">
+                        <div class="mb-4">
+
+                        </div>
+
+                    </div>
+                    <div class="flex flex-column align-items-center flex-or">
+                        <Avatar :image="user.props?.avatarUrl" alt="user avatar" class="h-10rem w-10rem" shape="circle" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+  
+<script setup lang="ts">
+//import { formatDate } from '~/app/dateTime';
+const route = useRoute();
+const dt = useDateTime();
+const user = new VingObject({
+    fetchApi: '/api/user/' + route.params.id,
+});
+await user.fetch();
+</script>
