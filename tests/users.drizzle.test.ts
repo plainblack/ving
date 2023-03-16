9f6872cbb231d11b80356d09bc0fe7f40486e218
JT Smith <jt@plainblack.com>
2023-03-16 10:24:49 -0500

    added where callback example

--- tests/users.drizzle.test.ts
+++ tests/users.drizzle.test.ts
@@ -1,9 +1,13 @@
 import { describe, test, expect } from "vitest";
 import { db } from '../drizzle/db';
-import { like, eq } from 'drizzle-orm/expressions';
+import { like, eq, asc, desc, and } from 'drizzle-orm/expressions';
 import { sql } from 'drizzle-orm';
-import { users } from '../drizzle/schema/users';
+import { ValueOrArray } from 'drizzle-orm/utils';
+import { AnyMySqlColumn } from 'drizzle-orm/mysql-core';
+import { MySqlSelectBuilder, MySqlSelect } from 'drizzle-orm/mysql-core/query-builders'
+import { users, User } from '../drizzle/schema/users';
 import type { SQL } from 'drizzle-orm/sql';
+import type { JoinNullability, SelectMode } from 'drizzle-orm/mysql-core/query-builders/select.types';
 
 
 describe('users', async () => {
@@ -32,9 +36,45 @@ describe('users', async () => {
         expect(result[0].realName).toBe('Warden');
     });
 
+    test("use a where callback to extend a query", async () => {
+        const startIt = (whereCallback: (condition: SQL) => SQL | undefined = (c) => c) => {
+            return db.select().from(users).where(whereCallback(eq(users.developer, true)));
+        }
+        const result = await startIt((c) => and(c, eq(users.admin, true)));
+        expect(result.length).toBe(0);
+    });
+
+    // can't figure out the types on this
+    /*
     test("can pass order by", async () => {
-        const passWhere = async (where: SQL) => await db.select().from(users).where(where);
-        const result = await passWhere(sql`username = 'warden'`);
+        const passOrderBy = async (orderBy: (SQL | AnyMySqlColumn)[]) => await db.select().from(users).orderBy(orderBy);
+        const result = await passOrderBy([asc(users.username), desc(users.realName)]);
+        expect(result[0].realName).toBe('Warden');
+    });
+    */
+
+    test("can pass order by with 2", async () => {
+        const passOrderBy = async (orderBy: SQL | AnyMySqlColumn) => await db.select().from(users).orderBy(orderBy);
+        const result = await passOrderBy(asc(users.username));
+        expect(result[0].realName).toBe('Warden');
+    });
+
+
+    test("can pass group by", async () => {
+        const passGroupBy = async (groupBy: SQL | AnyMySqlColumn) => await db.select().from(users).groupBy(groupBy);
+        const result = await passGroupBy(users.username);
+        expect(result[0].realName).toBe('Warden');
+    });
+
+    test("can pass select query", async () => {
+        const passSelect = async <
+            TTableName extends string,
+            TSelection,
+            TSelectMode extends SelectMode,
+            TExcludedMethods extends string,
+            TNullabilityMap extends Record<string, JoinNullability>,
+        >(qb: MySqlSelect<TTableName, TSelection, TSelectMode, TExcludedMethods, TNullabilityMap>) => await qb.limit(1);
+        const result = await passSelect(db.select().from(users).where(eq(users.id, 'a')));
         expect(result[0].realName).toBe('Warden');
     });
 
