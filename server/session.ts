87b85ff0b0fa96939c4fa23b62ee51dce2ba1c47
JT Smith <jt@plainblack.com>
2023-03-18 17:59:51 -0500

    session is class based

--- server/session.ts
+++ server/session.ts
@@ -1,10 +1,11 @@
 import { DescribeParams, Describe, RoleProps } from '../types';
 import { Users, UserRecord } from './vingrecord/records/Users';
-import { useVingRole, VingRole, RoleOptions } from './vingrecord/mixins/Role';
+import { RoleMixin, RoleOptions } from './vingrecord/mixins/Role';
 import { ouch } from './helpers';
 import { cache } from './cache';
 import { v4 } from 'uuid';
 
+/*
 export interface Session extends VingRole {
     _userObj: UserRecord | undefined,
     user(userObj?: UserRecord): Promise<UserRecord>,
@@ -15,95 +16,99 @@ export interface Session extends VingRole {
     start(user: UserRecord): Promise<Session>,
     fetch(id: string): Promise<Session>,
 }
+*/
 
-export function useSession(props: RoleProps, id = v4()) {
 
-    const getAll = () => {
-        return props;
+class ProtoSession {
+
+    constructor(private props: RoleProps, public id = v4()) { }
+
+
+    public get<K extends keyof RoleProps>(key: K): RoleProps[K] {
+        return this.props[key];
     }
 
-    const Session: Session = {
-
-        ...useVingRole({ getAll, props }),
-        getAll,
-
-        _userObj: undefined,
-
-        async user(userObj) {
-            if (userObj !== undefined) {
-                return this._userObj = userObj;
-            }
-            if (this._userObj !== undefined) {
-                return this._userObj;
-            }
-            return this._userObj = await Users.find(props.id) as UserRecord;
-        },
-
-        async end() {
-            await cache.delete('session-' + id);
-        },
-
-        async extend() {
-            const userChanged = await cache.get('user-changed-' + props.id);
-            if (userChanged) {
-                const user = await this.user();
-                if (props.password != user.get('password')) { // password changed since session created
-                    throw ouch(401, 'Session expired.');
-                }
-                else {
-                    for (const role of RoleOptions) {
-                        props[role] = user.get(role);
-                    }
-                }
-            }
-            await cache.set('session-' + id, props, 1000 * 60 * 60 * 24 * 7);
-        },
-
-        async describe(params = {}) {
-            const out: { props: { id: string, userId?: string }, related?: { user: Describe<'User'> }, links?: Record<string, string>, meta?: Record<string, any> } = {
-                props: {
-                    id: id,
-                    userId: this.getRoleProp('id'),
-                }
-            };
-            if ('include' in params && params.include !== undefined) {
-                if (params.include.related && params.include.related.length) {
-                    out.related = {
-                        user: await (await this.user()).describe(params)
-                    }
-                }
-                if (params.include.links) {
-                    out.links = {
-                        base: '/api/user/session',
-                        self: '/api/user/session/' + id,
-                    }
-                }
-                if (params.include.meta) {
-                    out.meta = {
-                        type: 'Session',
-                    }
-                }
-            }
-            return out;
-        },
-
-        async start(user) {
-            const session = useSession(user.getAll());
-            await session.extend();
-            return session;
-        },
-
-        async fetch(id: string) {
-            const data: RoleProps | undefined = await cache.get('session-' + id);
-            if (data !== undefined) {
-                return useSession(data, id);
-            }
-            throw ouch(401, 'Session expired.');
-        },
+    public getAll() {
+        return this.props;
     }
-    return Session;
 
+    private _userObj: UserRecord | undefined = undefined;
+
+    public async user(userObj?: UserRecord) {
+        if (userObj !== undefined) {
+            return this._userObj = userObj;
+        }
+        if (this._userObj !== undefined) {
+            return this._userObj;
+        }
+        return this._userObj = await Users.find(this.props.id) as UserRecord;
+    }
+
+    public async end() {
+        await cache.delete('session-' + this.id);
+    }
+
+    public async extend() {
+        const userChanged = await cache.get('user-changed-' + this.props.id);
+        if (userChanged) {
+            const user = await this.user();
+            if (this.props.password != user.get('password')) { // password changed since session created
+                throw ouch(401, 'Session expired.');
+            }
+            else {
+                for (const role of RoleOptions) {
+                    this.props[role] = user.get(role);
+                }
+            }
+        }
+        await cache.set('session-' + this.id, this.props, 1000 * 60 * 60 * 24 * 7);
+    }
+
+    public async describe(params: DescribeParams = {}) {
+        const out: { props: { id: string, userId?: string }, related?: { user: Describe<'User'> }, links?: Record<string, string>, meta?: Record<string, any> } = {
+            props: {
+                id: this.id,
+                userId: this.get('id'),
+            }
+        };
+        if ('include' in params && params.include !== undefined) {
+            if (params.include.related && params.include.related.length) {
+                out.related = {
+                    user: await (await this.user()).describe(params)
+                }
+            }
+            if (params.include.links) {
+                out.links = {
+                    base: '/api/user/session',
+                    self: '/api/user/session/' + this.id,
+                }
+            }
+            if (params.include.meta) {
+                out.meta = {
+                    type: 'Session',
+                }
+            }
+        }
+        return out;
+    }
+
+    public async start(user: UserRecord) {
+        const session = new Session(user.getAll());
+        await session.extend();
+        return session;
+    }
+
+    public async fetch(id: string) {
+        const data: RoleProps | undefined = await cache.get('session-' + id);
+        if (data !== undefined) {
+            return new Session(data, id);
+        }
+        throw ouch(401, 'Session expired.');
+    }
 }
+
+export class Session extends RoleMixin(ProtoSession) { }
+
 
 export const testSession = (session: Session | undefined) => {
     if (session === undefined) {
