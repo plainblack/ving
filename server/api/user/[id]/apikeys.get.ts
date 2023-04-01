08d27cdc880e316bc25d95faefd9e12f7db0ea3c
JT Smith <jt@plainblack.com>
2023-04-01 11:59:04 -0500

    added missing await

--- server/api/user/[id]/apikeys.get.ts
+++ server/api/user/[id]/apikeys.get.ts
@@ -4,5 +4,5 @@ export default defineEventHandler(async (event) => {
     const Users = useUsers();
     const { id } = getRouterParams(event);
     const user = await Users.findOrDie(id);
-    return user.apikeys.describeList(vingDescribeList(event));
+    return await user.apikeys.describeList(vingDescribeList(event));
 });
