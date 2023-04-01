0ec7d1b3561d55c1b7e017f6613a419607b4439c
JT Smith <jt@plainblack.com>
2023-04-01 11:58:52 -0500

    added user relationship

--- server/vingrecord/records/APIKey.ts
+++ server/vingrecord/records/APIKey.ts
@@ -1,14 +1,14 @@
 import { VingRecord, VingKind } from "../VingRecord";
 import { useDB } from '../../drizzle/db';
 import { APIKeyTable } from '../../drizzle/schema/APIKey';
-
+import { useUsers } from './User'
 
 export class APIKeyRecord extends VingRecord<'APIKey'> {
     // add custom Record code here
 
-    //  public get user(): any {
-    //    return Users.findUnique({ where: { id: this.get('userId') } });
-    //}
+    public get user(): any {
+        return useUsers().findOrDie(this.get('userId'));
+    }
 }
 
 export class APIKeyKind extends VingKind<'APIKey', APIKeyRecord>  {
