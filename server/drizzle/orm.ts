add2c93bd5ff9a2d4c9b806be59a475f3e835296
JT Smith <jt@plainblack.com>
2023-04-01 20:55:04 -0500

    use named exports to avoid conflicts

--- server/drizzle/orm.ts
+++ server/drizzle/orm.ts
@@ -1,10 +1,10 @@
 // This file exists because of
 // https://github.com/drizzle-team/drizzle-orm/issues/163
-export * from 'drizzle-orm/mysql2/index.js';
-export * from 'drizzle-orm/mysql-core/index.js';
+export { drizzle, MySql2Database } from 'drizzle-orm/mysql2/index.js';
+export { boolean, mysqlEnum, mysqlTable, timestamp, uniqueIndex, varchar, text, AnyMySqlColumn } from 'drizzle-orm/mysql-core/index.js';
 export { AnyMySqlColumnBuilder } from 'drizzle-orm/mysql-core/columns/common.js';
-export * from 'drizzle-orm/mysql-core/expressions.js';
+export { like, eq, asc, desc, and, or, ne, inArray } from 'drizzle-orm/mysql-core/expressions.js';
 export { SQL, sql } from 'drizzle-orm/sql/index.js';
 export { Name } from "drizzle-orm/table.js";
-export type { AnyMySqlSelect } from 'drizzle-orm/mysql-core/query-builders/select.types';
+export type { AnyMySqlSelect } from 'drizzle-orm/mysql-core/query-builders/select.types.js';
 export { ValueOrArray } from 'drizzle-orm/utils.js';
