17d7a6a6a3444584fd8b9e90bd078d1347fb666f
JT Smith <jt@plainblack.com>
2023-04-04 21:45:35 -0500

    remove debug

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,7 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const currentUserStore = useCurrentUserStore();
     const isAuthenticated = await currentUserStore.isAuthenticated();
-    console.log(to);
     if (!isAuthenticated) {
         return navigateTo(`/user/login?redirectAfter=${to.fullPath}`);
     }
