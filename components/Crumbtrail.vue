89aa8083c7fe5d79d56b3d7393d98b70829223f9
JT Smith <jt@plainblack.com>
2023-04-04 22:51:54 -0500

    make crumbtrails easily

---
+++ components/Crumbtrail.vue
@@ -0,0 +1,23 @@
+<template>
+    <Breadcrumb :model="props.crumbs">
+        <template #item="{ item }">
+            <NuxtLink :to="item.to">
+                {{ item.label }}
+            </NuxtLink>
+        </template>
+    </Breadcrumb>
+</template>
+
+<script setup lang="ts">
+import Breadcrumb from 'primevue/breadcrumb';
+
+const props = withDefaults(
+    defineProps<{
+        crumbs: { label: string, to?: string }[],
+    }>(),
+    {
+
+    }
+);
+
+</script>
