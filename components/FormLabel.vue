481f256a562a18234f87349081b881a43bef43b9
JT Smith <jt@plainblack.com>
2023-04-07 22:35:56 -0500

    added form label

---
+++ components/FormLabel.vue
@@ -0,0 +1,14 @@
+<template>
+    <label v-if="label" :for="id" class="block font-medium text-900 mb-2">{{ label }}</label>
+</template>
+
+<script setup lang="ts">
+const props = withDefaults(
+    defineProps<{
+        label?: string,
+        id?: string,
+    }>(),
+    {
+    }
+);
+</script>
