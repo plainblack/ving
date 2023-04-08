35fd3020c83586046fd257eecbca025c8fbdddc8
JT Smith <jt@plainblack.com>
2023-04-07 22:48:01 -0500

    added prepend/append and some other stuff

--- components/FormInput.vue
+++ components/FormInput.vue
@@ -1,21 +1,28 @@
 <template>
     <div :class="class">
         <FormLabel :label="label" :id="computedId" />
-        <InputNumber v-if="type == 'number' && (_.isNumber(val) || _.isNull(val) || _.isUndefined(val))" v-model="val"
-            showButtons :placeholder="placeholder" :name="name" :id="computedId" :autocomplete="autocomplete"
-            :required="required" :inputClass="fieldClass" />
-        <Password v-else-if="type == 'password' && (_.isString(val) || _.isNull(val) || _.isUndefined(val))" v-model="val"
-            toggleMask :placeholder="placeholder" :name="name" :id="computedId" :autocomplete="autocomplete"
-            :required="required" :inputClass="fieldClass" class="w-full" />
-        <TextArea v-else-if="type == 'textarea' && (_.isString(val) || _.isNull(val) || _.isUndefined(val))" v-model="val"
-            :placeholder="placeholder" :name="name" :id="computedId" :autocomplete="autocomplete" :class="fieldClass"
-            :required="required" />
-        <InputText v-else-if="['text', 'email'].includes(type) && (_.isString(val) || _.isNull(val) || _.isUndefined(val))"
-            v-model="val" :placeholder="placeholder" :name="name" :id="computedId" :autocomplete="autocomplete"
-            :class="fieldClass" :required="required" />
-        <Message v-else severity="error" :closable="false">
-            Can't display {{ displayName }} Form Input
-        </Message>
+        <div class="p-inputgroup flex-1">
+            <span v-if="prepend" class="p-inputgroup-addon"> {{ prepend }} </span>
+            <InputNumber v-if="type == 'number' && (_.isNumber(val) || _.isNull(val) || _.isUndefined(val))" v-model="val"
+                showButtons :placeholder="placeholder" :name="name" :id="computedId" :autocomplete="autocomplete"
+                :required="required" :inputClass="fieldClass" :step="step"
+                :incrementButtonClass="append ? 'border-noround' : ''"
+                :decrementButtonClass="append ? 'border-noround' : ''" />
+            <Password v-else-if="type == 'password' && (_.isString(val) || _.isNull(val) || _.isUndefined(val))"
+                v-model="val" toggleMask :placeholder="placeholder" :name="name" :id="computedId"
+                :autocomplete="autocomplete" :required="required" :inputClass="fieldClass" class="w-full" />
+            <TextArea v-else-if="type == 'textarea' && (_.isString(val) || _.isNull(val) || _.isUndefined(val))"
+                v-model="val" :placeholder="placeholder" :name="name" :id="computedId" :autocomplete="autocomplete"
+                :class="fieldClass + ' border-round'" :required="required" autoResize />
+            <InputText
+                v-else-if="['text', 'email'].includes(type) && (_.isString(val) || _.isNull(val) || _.isUndefined(val))"
+                v-model="val" :placeholder="placeholder" :name="name" :id="computedId" :autocomplete="autocomplete"
+                :class="fieldClass" :required="required" />
+            <Message v-else severity="error" :closable="false">
+                Can't display {{ displayName }} Form Input
+            </Message>
+            <span v-if="append" class="p-inputgroup-addon"> {{ append }} </span>
+        </div>
         <small :class="invalid && !empty ? 'text-red-500' : ''" v-if="invalid">{{ invalidReason }}</small>
     </div>
 </template>
@@ -29,10 +36,13 @@ const props = withDefaults(
         type?: 'textarea' | 'text' | 'password' | 'number' | 'email',
         name: string,
         id?: string,
+        append?: string,
+        prepend?: string,
         autocomplete?: string,
         modelValue: string | number | undefined | null,
         placeholder?: string,
         required?: boolean,
+        step: number,
         mustMatch?: { field: string, value: string | number | undefined | null } | undefined,
         class?: string,
     }>(),
@@ -41,6 +51,7 @@ const props = withDefaults(
         autocomplete: 'off',
         required: false,
         mustMatch: undefined,
+        step: 1,
     }
 );
 const computedId = props.id || props.name;
