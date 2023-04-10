540019b35ed99b4723004f215b74335e6f8b850c
JT Smith <jt@plainblack.com>
2023-04-09 21:40:46 -0500

    added schema generator

--- ving.ts
+++ ving.ts
@@ -8,8 +8,8 @@ const main = defineCommand({
     description: "ving CLI",
   },
   subCommands: {
-    build: () => import('./commands/build').then((r) => r.default),
-    deploy: () => import('./commands/deploy').then((r) => r.default),
+    // build: () => import('./commands/build').then((r) => r.default),
+    // deploy: () => import('./commands/deploy').then((r) => r.default),
     cache: () => import('./commands/cache').then((r) => r.default),
     drizzle: () => import('./commands/drizzle').then((r) => r.default),
     schema: () => import('./commands/schema').then((r) => r.default),
