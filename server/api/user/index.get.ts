5ebcb7716d35444f6386544f21475262c85adcb9
JT Smith <jt@plainblack.com>
2023-04-10 15:50:11 -0500

    use vingSessionIsRole

--- server/api/user/index.get.ts
+++ server/api/user/index.get.ts
@@ -1,11 +1,9 @@
 import { useUsers } from '../../vingrecord/records/User';
-import { vingDescribeList, vingSession, ouch } from '../../helpers';
+import { vingDescribeList, vingSessionIsRole } from '../../helpers';
 import { like, or } from '../../drizzle/orm';
 export default defineEventHandler(async (event) => {
-  const session = vingSession(event);
-  // comment the 2 lines below out if you want to allow mere users to access the user list
-  if (session === undefined || !session.isRole('admin'))
-    throw ouch(403, 'You must be an admin to do that.');
+  // comment the line below out if you want to allow mere users to access the user list
+  vingSessionIsRole(event, 'admin');
   const Users = useUsers();
   const query = getQuery(event);
   let where = undefined;
