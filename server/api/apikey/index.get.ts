98be692bd84f043b0a315dc62c33253d4c615084
JT Smith <jt@plainblack.com>
2023-04-10 20:26:13 -0500

    match codgen

--- server/api/apikey/index.get.ts
+++ server/api/apikey/index.get.ts
@@ -1,6 +1,6 @@
 import { useAPIKeys } from '../../vingrecord/records/APIKey';
 import { vingDescribeList, vingSession, ouch } from '../../helpers';
 export default defineEventHandler(async (event) => {
-  const APIKeys = useAPIKeys();
-  return await APIKeys.describeList(vingDescribeList(event));
-})
+    const APIKeys = useAPIKeys();
+    return await APIKeys.describeList(vingDescribeList(event));
+});
--- server/api/apikey/options.get.ts
+++ server/api/apikey/options.get.ts
@@ -3,4 +3,4 @@ import { vingDescribe } from '../../helpers';
 export default defineEventHandler(async (event) => {
     const APIKeys = useAPIKeys();
     return APIKeys.mint().propOptions(vingDescribe(event), true);
-})
+});
