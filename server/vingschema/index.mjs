aacd5072109c7965c618e3a56e4ce0a05ed2737b
JT Smith <jt@plainblack.com>
2024-01-31 15:04:59 -0600

    removing test schema

--- server/vingschema/index.mjs
+++ server/vingschema/index.mjs
@@ -1,9 +1,7 @@
 import { userSchema } from "./schemas/User.mjs";
 import { apikeySchema } from "./schemas/APIKey.mjs";
-import { testSchema } from "./schemas/Test.mjs";
 
 export const vingSchemas = [
     userSchema,
     apikeySchema,
-    testSchema,
 ];
