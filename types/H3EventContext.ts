50714ead16493e2621971be28665d43029738efd
JT Smith <jt@plainblack.com>
2024-01-31 15:00:35 -0600

    removed types

--- types/H3EventContext.ts
+++
@@ -1,6 +0,0 @@
-import type { Session } from '../server/session.mjs';
-declare module 'h3' {
-    interface H3EventContext {
-        ving: { session?: InstanceType<typeof Session> }
-    }
-}
--- types/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './vingschema';
-export * from './vingrecord';
-export * from './vingrecordclient';
-export * from './vingkindclient';
-export * from './util';
