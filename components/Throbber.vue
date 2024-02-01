915b07bdde6f4c6196753794d4ad50853f0effa5
JT Smith <jt@plainblack.com>
2024-02-01 09:31:38 -0600

    no reason to render the progress bar server side and can cause hydration errors

--- components/Throbber.vue
+++ components/Throbber.vue
@@ -1,7 +1,9 @@
 <template>
     <div id="wingajaxprogress" style="z-index: 10000; position: fixed; top: 0px; width: 100%;">
         <template v-if="throbber.counter > 0">
-            <ProgressBar mode="indeterminate" style="height: 5px"></ProgressBar>
+            <client-only>
+                <ProgressBar mode="indeterminate" style="height: 5px"></ProgressBar>
+            </client-only>
         </template>
     </div>
 </template>
