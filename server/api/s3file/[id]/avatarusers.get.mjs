6b09141c360caa0838b055849f5b448c3a0ba2eb
JT Smith <jt@plainblack.com>
2024-02-15 18:50:15 -0600

    fix casing problem

---
+++ server/api/s3file/[id]/avatarusers.get.mjs
@@ -0,0 +1,10 @@
+import { useS3Files } from '../../../vingrecord/records/S3File.mjs';
+import { describeListParams } from '../../../utils/rest.mjs';
+import {defineEventHandler, getRouterParams} from 'h3';
+export default defineEventHandler(async (event) => {
+    const S3Files = useS3Files();
+    const { id } = getRouterParams(event);
+    const s3file = await S3Files.findOrDie(id);
+    const Users = s3file.avatarUsers;
+    return await Users.describeList(describeListParams(event), describeListWhere(event, Users.describeListFilter()));
+});
