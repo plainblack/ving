763a8bd5d63ebcc79d70a15c5edc9e81a40bcc06
JT Smith <jt@plainblack.com>
2024-02-16 22:29:49 -0600

    rename commands to cli

--- ving.mjs
+++ ving.mjs
@@ -3,18 +3,18 @@ import { defineCommand, runMain } from "citty";
 
 const main = defineCommand({
   meta: {
-    name: "ving",
+    name: "ving.mjs",
     description: "ving CLI",
   },
   subCommands: {
-    cache: () => import('#ving/commands/cache.mjs').then((r) => r.default),
-    drizzle: () => import('#ving/commands/drizzle.mjs').then((r) => r.default),
-    email: () => import('#ving/commands/email.mjs').then((r) => r.default),
-    messagebus: () => import('#ving/commands/messagebus.mjs').then((r) => r.default),
-    record: () => import('#ving/commands/record.mjs').then((r) => r.default),
-    schema: () => import('#ving/commands/schema.mjs').then((r) => r.default),
-    token: () => import('#ving/commands/token.mjs').then((r) => r.default),
-    user: () => import('#ving/commands/user.mjs').then((r) => r.default),
+    cache: () => import('#ving/cli/cache.mjs').then((r) => r.default),
+    drizzle: () => import('#ving/cli/drizzle.mjs').then((r) => r.default),
+    email: () => import('#ving/cli/email.mjs').then((r) => r.default),
+    messagebus: () => import('#ving/cli/messagebus.mjs').then((r) => r.default),
+    record: () => import('#ving/cli/record.mjs').then((r) => r.default),
+    schema: () => import('#ving/cli/schema.mjs').then((r) => r.default),
+    token: () => import('#ving/cli/token.mjs').then((r) => r.default),
+    user: () => import('#ving/cli/user.mjs').then((r) => r.default),
   },
 });
 
