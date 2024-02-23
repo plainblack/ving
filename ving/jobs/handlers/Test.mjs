b7cfedbbd30894b2d71525e62d622d581b48c89a
JT Smith <jt@plainblack.com>
2024-02-23 10:39:45 -0500

    use ving index

--- ving/jobs/handlers/Test.mjs
+++ ving/jobs/handlers/Test.mjs
@@ -1,6 +1,6 @@
-import { log } from '#ving/log.mjs';
+import ving from '#ving/index.mjs';
 
 export default async function (job) {
-    log('agent').debug(`Test ran with data: ${JSON.stringify(job.data)}`);
+    ving.log('agent').debug(`Test ran with data: ${JSON.stringify(job.data)}`);
     return true;
 }
