2a0163dfd90cf4adfc2af676e07d71fd39932e3b
JT Smith <jt@plainblack.com>
2024-02-22 23:31:31 -0500

    added a central ving index for commonly shared functions

--- ving/index.mjs
+++ ving/index.mjs
@@ -1,5 +1,20 @@
 import { useCache } from '#ving/cache.mjs';
+import { log } from '#ving/log.mjs';
+import { sendMail } from '#ving/email/send.mjs';
+import { addJob } from '#ving/jobs/queue.mjs';
+import { ouch } from '#ving/utils/ouch.mjs';
+import { useKind } from '#ving/record/VingRecord.mjs'
+import { useDB } from '#ving/drizzle/db.mjs'
 
 export default {
     cache: useCache(),
+    log,
+    sendMail,
+    addJob,
+    ouch,
+    useKind,
+    close: async () => {
+        await useDB().session.client.pool.end();
+        await useCache().disconnect();
+    }
 }
