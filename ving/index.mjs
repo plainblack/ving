1a57598777ed7c58c4c410727f750d0da0fdaaff
JT Smith <jt@plainblack.com>
2024-02-24 18:42:25 -0500

    basic documentation for this index

--- ving/index.mjs
+++ ving/index.mjs
@@ -7,6 +7,19 @@ import { useKind } from '#ving/record/VingRecord.mjs'
 import { useDB } from '#ving/drizzle/db.mjs'
 import { useRedis } from '#ving/jobs/connection.mjs'
 
+/**
+ * Exports a list of the most useful features of Ving from a single import location.
+ * 
+ * @returns an object containing the following:
+ * 
+ * cache - the same as the result of `useCache()` from '#ving/cache.mjs'
+ * log() - the same as `log` from '#ving/log.mjs'
+ * sendMail() - the same as `sendMail` from '#ving/email/send.mjs'
+ * addJob() - the same as `addJob` from '#ving/jobs/queue.mjs'
+ * ouch() - the same as `ouch` from '#ving/utils/ouch.mjs'
+ * useKind() - the same as `useKind` from '#ving/record/VingRecord.mjs'
+ * close() - closes the connections mysql, redis/BullMQ, and redis/cache
+ */
 export default {
     cache: useCache(),
     log,
