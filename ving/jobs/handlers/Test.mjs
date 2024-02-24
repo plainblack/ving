b50c2ce6d0945fe15c94cb700fb7be7ef2c01476
JT Smith <jt@plainblack.com>
2024-02-24 13:14:55 -0500

    rename agent to jobs

--- ving/jobs/handlers/Test.mjs
+++ ving/jobs/handlers/Test.mjs
@@ -1,6 +1,6 @@
 import ving from '#ving/index.mjs';
 
 export default async function (job) {
-    ving.log('agent').debug(`Test ran with data: ${JSON.stringify(job.data)}`);
+    ving.log('jobs').debug(`Test ran with data: ${JSON.stringify(job.data)}`);
     return true;
 }
