7163b34743308d2524fba03a593260040fd3a9e1
JT Smith <jt@plainblack.com>
2024-02-25 18:06:22 -0600

    fix typo

--- ving/schema/map.mjs
+++ ving/schema/map.mjs
@@ -1,5 +1,5 @@
-import { findObject } from '#ving/utils/findObject';
-import { ouch } from '#ving/utils/findObject';
+import { findObject } from '#ving/utils/findObject.mjs';
+import { ouch } from '#ving/utils/ouch.mjs';
 import { userSchema } from "#ving/schema/schemas/User.mjs";
 import { apikeySchema } from "#ving/schema/schemas/APIKey.mjs";
 import { s3fileSchema } from "#ving/schema/schemas/S3File.mjs";
