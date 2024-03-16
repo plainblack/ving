418363136d45a12a06afaaafd159e86688d4c601
JT Smith <jt@plainblack.com>
2024-03-16 10:34:52 -0500

    cache config so it doesn't need to be read more than once

--- ving/config.mjs
+++ ving/config.mjs
@@ -1,11 +1,17 @@
 import { readFile } from 'fs/promises';
 
+let config = undefined;
 /**
  * Reads `ving.json`
  * @returns An object with the contents of `ving.json`
  */
-export const getConfig = async () => JSON.parse(
-    await readFile(
-        new URL('../ving.json', import.meta.url)
-    )
-);
+export const getConfig = async () => {
+    if (config !== undefined)
+        return config;
+    config = JSON.parse(
+        await readFile(
+            new URL('../ving.json', import.meta.url)
+        )
+    );
+    return config;
+}
