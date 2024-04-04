aa95b6fafb3699d89ab75b9d919467cbd87c8a19
JT Smith <jt@plainblack.com>
2024-04-04 15:57:37 -0500

    added documentation for the mediumText type

--- ving/drizzle/orm.mjs
+++ ving/drizzle/orm.mjs
@@ -3,6 +3,10 @@ export { boolean, mysqlEnum, mysqlTable, timestamp, uniqueIndex, varchar, text, 
 export { like, eq, asc, desc, and, or, ne, gt, gte, lt, lte, inArray, getTableName } from 'drizzle-orm';
 export { sql } from 'drizzle-orm/sql';
 
+/**
+ * Generates a `mediumtext` MySQL field through the Drizzle types system.
+ */
+
 export const mediumText = customType({
     dataType() {
         return 'mediumtext';
