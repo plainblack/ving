97f591898eca2814ea747e677b8803c6160ce650
JT Smith <jt@plainblack.com>
2024-04-20 18:11:00 -0500

    remove all formatting

--- layouts/empty.vue
+++ layouts/empty.vue
@@ -2,11 +2,7 @@
     <client-only>
         <SystemWideAlert />
     </client-only>
-    <div class="px-0 py-4 md:px-4">
-        <div style="min-height: 20rem">
-            <slot />
-        </div>
-    </div>
+    <slot />
     <client-only>
         <Notify />
         <Throbber />
