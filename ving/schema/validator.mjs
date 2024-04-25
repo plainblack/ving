816f8e05cb3bafc6aae78833a6a0bed2d062e987
JT Smith <jt@plainblack.com>
2024-04-25 16:29:10 -0500

    built a ving schema validator

--- ving/schema/validator.mjs
+++ ving/schema/validator.mjs
@@ -1,7 +1,8 @@
 import { vingSchemas } from './map.mjs';
-import { isUndefined, isString, isArray, isBoolean, isFunction, isNumber } from '#ving/utils/identify.mjs';
+import { isUndefined, isString, isArray, isBoolean, isFunction, isNumber, isObject } from '#ving/utils/identify.mjs';
 import ving from '#ving/index.mjs';
 import { RoleOptions } from './schemas/User.mjs';
+import { extensionMap } from './schemas/S3File.mjs';
 
 /**
  * Validates all Ving Schemas.
@@ -56,17 +57,216 @@ export const validateProps = (schema) => {
         validatePropType(prop, schema);
         validatePropName(prop, schema);
         validatePropRequired(prop, schema);
-        validatePropFilterQuery(prop, schema);
         validatePropDefault(prop, schema);
         validatePropDb(prop, schema);
         validatePropZod(prop, schema);
         validatePropView(prop, schema);
         validatePropEdit(prop, schema);
-        // validate relation
+        validatePropRelation(prop, schema);
         validatePropEnums(prop, schema);
         validatePropEnumLabels(prop, schema);
         validatePropLength(prop, schema);
+        validatePropUnique(prop, schema);
+        validatePropUniqueQualfiers(prop, schema);
+        validatePropFilterQuery(prop, schema);
+        validatePropFilterQualifier(prop, schema);
+        validatePropAutoUpdate(prop, schema);
     }
+}
+
+/**
+ * Validates the relation field of a prop.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validatePropRelation(prop, schema)
+ */
+export const validatePropRelation = (prop, schema) => {
+    if (!['id', 'virtual'].includes(prop.type)) {
+        if ('relation' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation should not exist.`);
+        return;
+    }
+    if (prop.type == 'id' && !('relation' in prop)) // not required
+        return;
+    if (!isObject(prop.relation))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation must be an object.`);
+    validateRelationType(prop, schema);
+    validateRelationKind(prop, schema);
+    validateRelationName(prop, schema);
+    validateRelationSkipOwnerCheck(prop, schema);
+    validateRelationAcceptedFileExtensions(prop, schema);
+}
+
+/**
+ * Validates the acceptedFileExtensions attribute of a prop relation.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validateRelationAcceptedFileExtensions(prop, schema)
+ */
+export const validateRelationAcceptedFileExtensions = (prop, schema) => {
+    if (!['S3File'].includes(prop.relation.kind) // excluded by kind
+        || !['parent'].includes(prop.relation.type) // excluded by type
+    ) {
+        if ('acceptedFileExtensions' in prop.relation)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.acceptedFileExtensions should not exist.`);
+        return;
+    }
+    if (!('acceptedFileExtensions' in prop.relation)) // optional
+        return;
+    if (!isArray(prop.relation.acceptedFileExtensions))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.acceptedFileExtensions must be an array.`);
+    if (prop.relation.acceptedFileExtensions.length == 0)
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.acceptedFileExtensions must have at least 1 file extension.`);
+    const allowedExtensions = Object.keys(extensionMap);
+    for (const ext of prop.relation.acceptedFileExtensions) {
+        if (!allowedExtensions.includes(ext))
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.acceptedFileExtensions value ${ext} is not an extension listed in the S3File extensionMap.`);
+    }
+}
+
+/**
+ * Validates the skipOwnerCheck attribute of a prop relation.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validateRelationSkipOwnerCheck(prop, schema)
+ */
+export const validateRelationSkipOwnerCheck = (prop, schema) => {
+    if (!['parent'].includes(prop.relation.type)) {// excluded by type
+        if ('skipOwnerCheck' in prop.relation)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.skipOwnerCheck should not exist.`);
+        return;
+    }
+    if (!('skipOwnerCheck' in prop.relation)) // optional
+        return;
+    if (!isBoolean(prop.relation.skipOwnerCheck))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.skipOwnerCheck must be a boolean.`);
+}
+
+/**
+ * Validates the name attribute of a prop relation.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validateRelationName(prop, schema)
+ */
+export const validateRelationName = (prop, schema) => {
+    if (!isString(prop.relation.name))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.name must be a string.`);
+    if (!/^[a-z]/.test(prop.relation.name))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.name must start with a lower case letter.`);
+}
+
+/**
+ * Validates the kind attribute of a prop relation.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validateRelationKind(prop, schema)
+ */
+export const validateRelationKind = (prop, schema) => {
+    if (!isString(prop.relation.kind))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.kind must be a string.`);
+    const otherSchema = vingSchemas.find(s => s.kind == prop.relation.kind);
+    if (isUndefined(otherSchema))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.kind refers to a schema that doesn't exist.`);
+    if (prop.relation.type == 'child') {
+        const childProp = otherSchema.props.find(p => p.name == prop.name);
+        if (isUndefined(childProp))
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name} refers to ${prop.relation.kind}.props.${prop.name} which does not exist.`);
+    }
+}
+
+/**
+ * Validates the type attribute of a prop relation.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validateRelationType(prop, schema)
+ */
+export const validateRelationType = (prop, schema) => {
+    if (!isString(prop.relation.type))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.type must be a string.`);
+    if (!['child', 'parent'].includes(prop.relation.type))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.type must either child or parent.`);
+    if (prop.relation.type == 'parent' && prop.type != 'id')
+        throw ving.ouch(442, `${schema.kind}.props.type must be id when ${schema.kind}.props.${prop.name}.relation.type is parent.`);
+    if (prop.relation.type == 'child' && prop.type != 'virtual')
+        throw ving.ouch(442, `${schema.kind}.props.type must be virtual when ${schema.kind}.props.${prop.name}.relation.type is child.`);
+}
+
+/**
+ * Validates the autoUpdate field of a prop.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validatePropAutoUpdate(prop, schema)
+ */
+export const validatePropAutoUpdate = (prop, schema) => {
+    if (!['date'].includes(prop.type)) {
+        if ('autoUpdate' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.autoUpdate should not exist.`);
+        return;
+    }
+    if (!('autoUpdate' in prop)) // not required
+        return;
+    if (!isBoolean(prop.autoUpdate))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.autoUpdate must be a boolean.`);
+}
+
+/**
+ * Validates the uniqueQualifiers field of a prop.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validatePropUniqueQualfiers(prop, schema)
+ */
+export const validatePropUniqueQualfiers = (prop, schema) => {
+    if (!('uniqueQualifiers' in prop)) // optional
+        return
+    if (prop.unique != true)
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.uniqueQualifiers should not exist.`);
+    if (!isArray(prop.uniqueQualifiers))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.uniqueQualifiers must be an array.`);
+    if (prop.uniqueQualifiers.length == 0)
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.uniqueQualifiers must have at least one member.`);
+    for (const qualifier of prop.uniqueQualifiers) {
+        const otherProp = schema.props.find(p => p.name == qualifier);
+        if (isUndefined(otherProp))
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.uniqueQualifiers value ${qualifier} does not match another prop name.`);
+        if (otherProp.name == prop.name)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.uniqueQualifiers value ${qualifier} references its own prop.`);
+    }
+}
+
+/**
+ * Validates the unique field of a prop.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validatePropUnique(prop, schema)
+ */
+export const validatePropUnique = (prop, schema) => {
+    if (!['string'].includes(prop.type)) {
+        if ('unique' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.unique should not exist.`);
+        return;
+    }
+    if (!('unique' in prop)) // optional
+        return
+    if (!isBoolean(prop.unique))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.unique must be a boolean.`);
 }
 
 /**
@@ -78,8 +278,11 @@ export const validateProps = (schema) => {
  * validatePropLength(prop, schema)
  */
 export const validatePropLength = (prop, schema) => {
-    if (!['id', 'string', 'enum'].includes(prop.type)) // exempted by type
+    if (!['id', 'enum', 'string'].includes(prop.type)) {
+        if ('length' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.length should not exist.`);
         return;
+    }
     if (!isNumber(prop.length))
         throw ving.ouch(442, `${schema.kind}.props.${prop.name}.length must be a number.`);
     if (prop.length < 1)
@@ -95,8 +298,11 @@ export const validatePropLength = (prop, schema) => {
  * validatePropEnumLabels(prop, schema)
  */
 export const validatePropEnumLabels = (prop, schema) => {
-    if (!['enum', 'boolean'].includes(prop.type)) // exempted by type
+    if (!['enum', 'boolean'].includes(prop.type)) {
+        if ('enumLabels' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.enumLabels should not exist.`);
         return;
+    }
     if (!isArray(prop.enumLabels))
         throw ving.ouch(442, `${schema.kind}.props.${prop.name}.enumLabels must be an array.`);
     if (prop.enums.length != prop.enumLabels.length)
@@ -116,8 +322,11 @@ export const validatePropEnumLabels = (prop, schema) => {
  * validatePropEnums(prop, schema)
  */
 export const validatePropEnums = (prop, schema) => {
-    if (!['enum', 'boolean'].includes(prop.type)) // exempted by type
+    if (!['enum', 'boolean'].includes(prop.type)) {
+        if ('enums' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.enums should not exist.`);
         return;
+    }
     if (!isArray(prop.enums))
         throw ving.ouch(442, `${schema.kind}.props.${prop.name}.enums must be an array.`);
     if (prop.enums.length == 0)
@@ -196,7 +405,6 @@ export const validatePropZod = (prop, schema) => {
         throw ving.ouch(442, `${schema.kind}.props.${prop.name}.zod must be a function that returns a zod validation function.`);
 }
 
-
 /**
  * Validates the db field of a prop.
  * @param {object} prop The prop schema to check against.
@@ -206,8 +414,11 @@ export const validatePropZod = (prop, schema) => {
  * validatePropDb(prop, schema)
  */
 export const validatePropDb = (prop, schema) => {
-    if (['virtual'].includes(prop.type))
+    if (['virtual'].includes(prop.type)) {
+        if ('db' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.db should not exist.`);
         return;
+    }
     if (!isFunction(prop.db))
         throw ving.ouch(442, `${schema.kind}.props.${prop.name}.db must be a function that returns a drizzle column specification.`);
 }
@@ -240,10 +451,55 @@ export const validatePropDefault = (prop, schema) => {
  * validatePropFilterQuery(prop, schema)
  */
 export const validatePropFilterQuery = (prop, schema) => {
+    if (!['enum', 'string'].includes(prop.type)) {
+        if ('filterQuery' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.filterQuery should not exist.`);
+        return;
+    }
     if (!('filterQuery' in prop)) // not required
         return;
     if (!isBoolean(prop.filterQuery))
         throw ving.ouch(442, `${schema.kind}.props.${prop.name}.filterQuery must be a boolean.`);
+}
+
+/**
+ * Validates the filterQualifier field of a prop.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validatePropFilterQualifier(prop, schema)
+ */
+export const validatePropFilterQualifier = (prop, schema) => {
+    if (!['enum', 'string', 'int', 'boolean', 'id'].includes(prop.type)) {
+        if ('filterQualifier' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.filterQualifier should not exist.`);
+        return;
+    }
+    if (!('filterQualifier' in prop)) // not required
+        return;
+    if (!isBoolean(prop.filterQualifier))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.filterQualifier must be a boolean.`);
+}
+
+/**
+ * Validates the filterRange field of a prop.
+ * @param {object} prop The prop schema to check against.
+ * @param {VingSchema} schema The schema to check against.
+ * @throws 442 if some attribute is outside of the normal definition
+ * @example
+ * validatePropFilterRange(prop, schema)
+ */
+export const validatePropFilterRange = (prop, schema) => {
+    if (!['int', 'date'].includes(prop.type)) {
+        if ('filterRange' in prop)
+            throw ving.ouch(442, `${schema.kind}.props.${prop.name}.filterRange should not exist.`);
+        return;
+    }
+    if (!('filterRange' in prop)) // not required
+        return;
+    if (!isBoolean(prop.filterRange))
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.filterRange must be a boolean.`);
 }
 
 /**
@@ -286,6 +542,8 @@ export const validatePropType = (prop, schema) => {
     const valid = ['boolean', 'int', 'date', 'enum', 'id', 'string', 'json', 'virtual']
     if (!valid.includes(prop.type))
         throw ving.ouch(442, `${schema.kind}.props.${prop.name}.type is invalid: ${prop.type}`);
+    if (prop.type == 'virtual' && prop.relation.type != 'child')
+        throw ving.ouch(442, `${schema.kind}.props.${prop.name}.relation.type must be child when ${schema.kind}.props.type is virtual.`);
 }
 
 
