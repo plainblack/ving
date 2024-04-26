85336593757fd0ef15bedd32dd3f132e0d8679db
JT Smith <jt@plainblack.com>
2024-04-26 14:16:07 -0500

    fix hydration mismatch on throbber

--- components/Throbber.vue
+++ components/Throbber.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="wingajaxprogress" style="z-index: 10000; position: fixed; top: 0px; width: 100%;">
         <template v-if="throbber.counter > 0">
-            <ProgressBar mode="indeterminate" style="height: 5px"></ProgressBar>
+            <ProgressBar id="throbberprogressbar" mode="indeterminate" style="height: 5px"></ProgressBar>
         </template>
     </div>
 </template>
