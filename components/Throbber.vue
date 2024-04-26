e0254b40b6005d3270d36019af9948b68787a4dd
JT Smith <jt@plainblack.com>
2024-04-26 14:16:55 -0500

    change id

--- components/Throbber.vue
+++ components/Throbber.vue
@@ -1,5 +1,5 @@
 <template>
-    <div id="wingajaxprogress" style="z-index: 10000; position: fixed; top: 0px; width: 100%;">
+    <div id="vingthrobber" style="z-index: 10000; position: fixed; top: 0px; width: 100%;">
         <template v-if="throbber.counter > 0">
             <ProgressBar id="throbberprogressbar" mode="indeterminate" style="height: 5px"></ProgressBar>
         </template>
