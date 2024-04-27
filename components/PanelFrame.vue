e02e357300be1ccb8f96cce6590fcd00217cb75b
JT Smith <jt@plainblack.com>
2024-04-26 23:48:58 -0500

    fix spacing issue

--- components/PanelFrame.vue
+++ components/PanelFrame.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="p-fluid flex flex-column lg:flex-row">
-        <div class="lg:pr-5">        
+        <div v-if="$slots.left" class="lg:pr-5">        
             <slot name="left"></slot>
         </div>
         <div class="flex-auto">
             <slot name="content"></slot>
         </div>
-        <div class="lg:pl-5 mt-1 lg:mt-0">        
+        <div v-if="$slots.right" class="lg:pl-5 mt-1 lg:mt-0">        
             <slot name="right"></slot>
         </div>
     </div>
