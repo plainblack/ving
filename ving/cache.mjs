8565b8028f19735936c44d3c4ca36801add399ce
JT Smith <jt@plainblack.com>
2024-10-02 12:32:23 -0500

    adding logging

--- ving/cache.mjs
+++ ving/cache.mjs
@@ -1,5 +1,6 @@
 import Keyv from 'keyv';
 import { ouch } from '#ving/utils/ouch.mjs';
+import { log } from '#ving/utils/log.mjs';
 let cache = undefined;
 
 /**
@@ -10,14 +11,20 @@ export const useCache = () => {
     if (cache) {
         return cache
     }
+    log('cache').info('Connecting to cache');
     const globalForKeyv = global;
+    log('cache').info('redis url' + process.env.VING_REDIS);
 
     cache = globalForKeyv.keyv || new Keyv(process.env.VING_REDIS);
 
     if (process.env.NODE_ENV !== 'production')
-        globalForKeyv.keyv = cache;
+        log('cache').info('got here, this could be the problem');
+    log('cache').debug(JSON.stringify(cache));
+    log('cache').debug(JSON.stringify(globalForKeyv));
+    globalForKeyv.keyv = cache;
 
     cache.on('error', () => {
+        log('cache').info('Error connecting to Cache' + process.env.VING_REDIS);
         throw ouch(504, 'Error connecting to Cache');
     });
 
