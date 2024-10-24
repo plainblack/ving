959589913350c7e190bb7e4a3f6fc22d8f411d93
JT Smith <jt@plainblack.com>
2024-10-24 12:47:45 -0500

    Updated the redis client to use dotenv to get the connection string.  * Updated the redis client to trap bad connection strings with an error message.

--- ving/cache.mjs
+++ ving/cache.mjs
@@ -1,5 +1,8 @@
 import Keyv from 'keyv';
 import { ouch } from '#ving/utils/ouch.mjs';
+import 'dotenv/config';
+import '@keyv/redis';
+
 let cache = undefined;
 
 /**
@@ -11,8 +14,11 @@ export const useCache = () => {
         return cache
     }
     const globalForKeyv = global;
-
-    cache = globalForKeyv.keyv || new Keyv(process.env.VING_REDIS);
+    const redisUrl = process.env.VING_REDIS || '';
+    if (!redisUrl) {
+        throw new Error('VING_REDIS environment variable is not set');
+    }
+    cache = globalForKeyv.keyv || new Keyv(redisUrl);
 
     if (process.env.NODE_ENV !== 'production')
         globalForKeyv.keyv = cache;
