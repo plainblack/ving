230b53b5e054b11b0c73fd520352985ed46c1275
JT Smith <jt@plainblack.com>
2024-10-30 11:35:04 -0500

    theoretically the more proper way to redirect after an error

--- error.vue
+++ error.vue
@@ -7,9 +7,7 @@
                 <div class="mt-6 mb-3 font-bold text-6xl text-900">{{ error?.statusMessage || "" }}</div>
                 <p class="text-700 text-3xl mt-0 mb-6">{{ error?.message || "" }}</p>
                 <p class="text-700 text-3xl mt-0 mb-6">{{ error?.description || "" }}</p>
-                <NuxtLink to="/" v-slot="{ navigate }" custom>
-                    <Button @mousedown="navigate" type="button" label="Go back to home page"></Button>
-                </NuxtLink>
+                    <Button @mousedown="clearError('/')" type="button" label="Go back to home page"></Button>
             </div>
             <div>
                 <img :src="config.public.site.logoUrl" :alt="config.public.site.name" class="w-full md:w-28rem" />
