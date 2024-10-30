124c3792c4e046fa8cb14aaa84a197c8fee0b2b8
JT Smith <jt@plainblack.com>
2024-10-30 12:00:24 -0500

    set status code on error

--- app/middleware/admin.mjs
+++ app/middleware/admin.mjs
@@ -1,6 +1,6 @@
 export default defineNuxtRouteMiddleware(async (to, from) => {
     const currentUser = useCurrentUser();
     if (!currentUser.props?.admin) {
-        return abortNavigation('You must be an admin to view this.');
+        return abortNavigation({ statusCode: 403, fatal: true, message: 'You must be an admin to view this.' });
     }
 });
